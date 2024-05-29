50c5065ef3d258b815c3436911b7a6a46fac4a56
Will Sackfield <will.sackfield@gmail.com>
2024-05-30 07:26:28 +1000

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='fullmonte',
-    version='0.0.1',
+    version='0.0.2',
     description='A monte carlo simulation for extrapolated returns.',
     long_description=long_description,
     long_description_content_type='text/markdown',
